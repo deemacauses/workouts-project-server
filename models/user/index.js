c3d82d14364105090f8299eefc4f437376e9276b
deemacauses <deemacauses@outlook.com>
2022-07-31 13:48:46 +0300

    New: Authentication - Email and Password Validation

--- models/user/index.js
+++ models/user/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
+const validator = require("validator")
 
 const Schema = mongoose.Schema
 const userSchema = new Schema({
@@ -16,6 +17,12 @@ const userSchema = new Schema({
 
 // Static sign-up method
 userSchema.statics.signUp = async function (email, password) {
+  // Validation
+  if (!email || !password) throw Error("All fields must be filled")
+  if (!validator.isEmail(email)) throw Error("Email is not valid")
+  if (!validator.isStrongPassword(password))
+    throw Error("Password not strong enough")
+
   const exists = await this.findOne({ email })
   if (exists) throw Error("Email already in use")
   const salt = await bcrypt.genSalt(10)
