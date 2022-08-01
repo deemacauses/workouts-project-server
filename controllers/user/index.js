19cc7280ce05ef42cb5abff17a1f8f28912f4554
deemacauses <deemacauses@outlook.com>
2022-08-01 17:43:21 +0300

    New: Authentication  - Signing Tokens

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,4 +1,9 @@
 const User = require("../../models/user/")
+const jwt = require("jsonwebtoken")
+
+const createToken = _id => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" })
+}
 
 // Log-in user
 const loginUser = async (request, response) => {
@@ -10,7 +15,9 @@ const signUpUser = async (request, response) => {
   try {
     const { email, password } = request.body
     const user = await User.signUp(email, password)
-    response.status(200).json({ email, user })
+    // Create a token
+    const token = createToken(user._id)
+    response.status(200).json({ email, token })
   } catch (error) {
     response.status(400).json({ error: error.message })
   }
