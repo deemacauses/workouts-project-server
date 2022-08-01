b6a2604cff574070b91ed440c02ff3ddf249ef02
deemacauses <deemacauses@outlook.com>
2022-08-01 18:20:06 +0300

    New: Authentication - Logging Users In

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -7,7 +7,15 @@ const createToken = _id => {
 
 // Log-in user
 const loginUser = async (request, response) => {
-  response.json({ message: "Login user" })
+  try {
+    const { email, password } = request.body
+    const user = await User.logIn(email, password)
+    // Create a token
+    const token = createToken(user._id)
+    response.status(200).json({ email, token })
+  } catch (error) {
+    response.status(400).json({ error: error.message })
+  }
 }
 
 // Sign-up user
--- models/user/index.js
+++ models/user/index.js
@@ -31,4 +31,14 @@ userSchema.statics.signUp = async function (email, password) {
   return user
 }
 
+// Static log-in method
+userSchema.statics.logIn = async function (email, password) {
+  if (!email || !password) throw Error("All fields must be filled")
+  const user = await this.findOne({ email })
+  if (!user) throw Error("Incorrect email")
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) throw Error("Incorrect password")
+  return user
+}
+
 module.exports = mongoose.model("User", userSchema)
